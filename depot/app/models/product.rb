b4c7e4d6cc678d421556d7d5d859dec5f3c1a585
myhgew <yuhua.mai@duke.edu>
2014-04-22 14:37:59 -0400

    catalog display

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	# alphabetical order
+	default_scope {order('title')}
 
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
