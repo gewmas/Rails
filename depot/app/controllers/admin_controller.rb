50a74f11c1adbfcec3ab430a3cb58a2cf5854579
myhgew <yuhua.mai@duke.edu>
2014-04-28 22:13:34 -0400

    finish log in

--- depot/app/controllers/admin_controller.rb
+++ depot/app/controllers/admin_controller.rb
@@ -6,12 +6,8 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class StoreController < ApplicationController
-	skip_before_action :authorize
-
-	include CurrentCart
-	before_action :set_cart
-	def index
-		@products = Product.order(:title)
-	end
+class AdminController < ApplicationController
+  def index
+    @total_orders = Order.count
+  end
 end
--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -6,12 +6,16 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class StoreController < ApplicationController
-	skip_before_action :authorize
+class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
 
-	include CurrentCart
-	before_action :set_cart
-	def index
-		@products = Product.order(:title)
-	end
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end     
 end
